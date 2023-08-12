79598838b746fc7cfed49d3a07c99d443d03fe9a
Narges-Safaeefard <139851899+Narges-Safaeefard@users.noreply.github.com>
2023-08-12 00:07:49 -0700

    Add files via upload

---
+++ test22.py
@@ -0,0 +1,102 @@
+m = [{"name":"neda",
+     "familly":"nzeda",
+     "city":"yazd",
+     "ave":19.5,
+     "age":37,
+     "email":"neda@gimel.com"},
+     {"name":"sara",
+     "familly":"zamani"}
+     ]
+print(m[0]["name"],m[1]["familly"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
